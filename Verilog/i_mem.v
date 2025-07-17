19669febc9bd2c60d8268f2a9553f222830410ae
Pedro Henrique <166336814+Pedro9820@users.noreply.github.com>
2025-07-17 20:43:13 -0300

    Leitura de instruções do arquivo

--- Verilog/i_mem.v
+++ Verilog/i_mem.v
@@ -1,12 +1,25 @@
 module i_mem(
-	input wire [31:0] Address,
-	output wire [31:0] i_out
-
+	Address,
+	i_out,
 );
 
-// esse modulo não faz nada além de receber o endereço, e enviar no fio de saida os dados lidos daquele endereco
-reg [31:0] memoria[0:1023]; // 4kb de memoria
-
-assign i_out = memoria[Address[11:2]]; //slice da sequencia de 32bits, para ler o endereco no vetor de memoria
+	// parametros para criação da memoria
+	parameter tamanho = 32; // entrada de bits do mips
+	parameter enderecamento = 10; // enderecamento de palavra de 4 bits
+	localparam tamanhoVetor = 1 << enderecamento; // tamanho do vetor é calculado com 2 ^ enderecamento
+	
+	input wire [tamanho - 1:0] Address;
+	output wire [tamanho - 1:0] i_out;
+	
+	//inicializa o vetor de memoria com as instruções binárias que vai ler do arquivo
+	reg [tamanho - 1:0] memoria [0: tamanhoVetor - 1];
+	
+	// leitura do arquivo de teste instruction.list
+	initial begin
+		// lê o valor binário desse arquivo
+		$readmemb("instruction.list", memoria);
+	end
+	
+assign i_out = memoria[Address[enderecamento + 1 :2]]; //slice da sequencia de 32bits, para ler o endereco no vetor de memoria
 
 endmodule 
